28b19519b5c019e60a15f7f00e7b6bbc82d37388
Edgarumma <102556623+Edgarumma@users.noreply.github.com>
2022-03-29 09:01:28 -0600

    Update sw.js

--- texto/sw.js
+++ texto/sw.js
@@ -1,15 +1,17 @@
 ;
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_cache_programador_fitness',
+const CACHE_NAME = 'v1_cache_matriculas_umma',
   urlsToCache = [
     './',
     'https://fonts.googleapis.com/css?family=Raleway:400,700',
     'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
     'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
-        './style.css',
+    'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
+    './style.css',
     './lectorqr.js',
     './script.js',
     './perfil',
+
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
